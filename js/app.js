4f268391c0f450694fd7802b4c127244f88a5c10
lexus600 <LexusToyota2002@gmail.com>
2024-01-22 09:48:57 +0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,50 @@
+
+//1-masala
+
+// let a = prompt("1-Son kiriting ")
+// let b = prompt("2-Son kiriting ")
+
+// if(a > b){
+//     alert(a)
+// }
+
+// else {
+//     alert(b)
+// }
+
+
+
+//2-masala
+
+// let a = prompt("1-son  ")
+// let b = prompt("2-son  ")
+// let c = prompt("3-son  ")
+
+// if(a>b && b>c){
+//     alert(a)
+// }
+// else if(c>b && b>a){
+//     alert (c)
+// }
+// else if (b>c && c>a){
+//     alert (b)
+// }
+
+
+// 3-masala
+
+// let a = prompt("Ism kiriting")
+
+// if (a == "Sardorbek"){
+//     console.log("JR Frontent Developer")
+// }
+
+// else if(a == "Hoshimjon"){
+//     console.log("JR html, Css");
+// }
+// else if(a == "Nurbek"){
+//     console.log("Universal");
+// }
+
+//4-masala
+
